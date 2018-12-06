c522b43e7c5186bd7b3dbbabf7428f9799fda2da
hongqingWang <272338444@qq.com>
2018-12-06 10:53:59 +0800

    冒泡排序
    冒泡排序

--- 04-冒泡排序/04-冒泡排序/main.c
+++ 04-冒泡排序/04-冒泡排序/main.c
@@ -9,7 +9,25 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int arr[5] = {5, 4, 3, 2, 1};
+    int total = 5;
+    int temp;
+    
+    for (int i = 1; i < total; i++) {
+        for (int j = 0; j < total - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    
+    for (int i = 0; i < total; i++) {
+        printf("%d\t", arr[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
